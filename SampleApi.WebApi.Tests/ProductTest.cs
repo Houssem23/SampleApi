8083c0f056a13fca09bb03f5c33b153fd39fb7f4
Houssem E Cherifi <houssem.cherifi@wolterskluwer.com>
2022-09-06 16:57:29 +0200

    Update ProductTest.cs

--- SampleApi.WebApi.Tests/ProductTest.cs
+++ SampleApi.WebApi.Tests/ProductTest.cs
@@ -22,8 +22,9 @@ namespace SampleApi.WebApi.Tests
         public async Task AddProductReturnsOkStatus()
         {
             const int Id = 1;
-
-            var response = await AddProductData(Id);
+           var response =  await _client.GetAsync($"/api/products");
+           Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            //var response = await AddProductData(Id);
 
             //Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
